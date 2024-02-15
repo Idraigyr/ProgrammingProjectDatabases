1a688885f4909f5c956ab2a305fe37c33c506178
Joren2087 <3331589-joren2087@users.noreply.gitlab.com>
2024-02-15 21:30:39 +0100

    Added Dotenv support, for environment specific variables

--- src/Backend/config.py
+++ src/Backend/config.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv("../../.env")
+from os import environ
+
 config_data = dict()
 config_data['app_name'] = 'ProgDB Tutor'
-config_data['dbname'] = 'dbtutor'
-config_data['dbuser'] = 'app'
-config_data['host'] = 'ubuntu-server.local'
+config_data['dbname'] = environ.get('POSTGRES_DATABASE')
+config_data['dbuser'] = environ.get('POSTGRES_USER')
+config_data['host'] = environ.get('POSTGRES_HOST')
