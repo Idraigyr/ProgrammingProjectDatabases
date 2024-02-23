5ebd08d7a5172df7443247b54f5fe10186ecda90
darik0d <70072471+darik0d@users.noreply.github.com>
2024-02-23 19:22:07 +0100

    Landing page (#2)
    * Assets and stuff for landing page
    
    * MVP landing page
    
    * Add redirection and custom cursor
    
    * Add some comments

--- src/routes/public_routes.py
+++ src/routes/public_routes.py
@@ -1,5 +1,5 @@
 from flask.templating import render_template
-from flask import Blueprint, current_app
+from flask import Blueprint, current_app, redirect
 import src.test_entity
 
 public_routes = Blueprint('public_routes', __name__)
@@ -7,8 +7,14 @@ db = current_app.db
 
 @public_routes.route("/")
 def main():
+    # TODO: remove this
+    return redirect("/landing", code=302)
     test = src.test_entity.TestEntity("test1234")
     db.session.add(test)
     db.session.commit()
     return render_template('index.html', app_name=current_app.config['APP_NAME'])
+
+@public_routes.route("/landing")
+def landing():
+    return render_template('landing-page.html', app_name=current_app.config['APP_NAME'])
 
