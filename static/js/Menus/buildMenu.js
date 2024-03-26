50d69b55ae63f63051d39cc651e0ba376477dcfd
Thomig <Thomas.Urkens@student.uantwerpen.be>
2024-03-26 22:09:36 +0100

    Build menu small update

--- static/js/Menus/buildMenu.js
+++ static/js/Menus/buildMenu.js
@@ -1,15 +1,41 @@
 // variables
 
 // lists buildings
-let buildings = ["tree", "tower"];
+let combat = ["tower"];
+let resources = ["fusionTable", "mine"];
+let decorations = ["tree", "bush"];
 
-function populateContainer(containerId, itemList) {
+// Function to show buildings or items based on the selected tab
+function showBuildings(type) {
+    const container = document.getElementById('container1');
+    container.innerHTML = ''; // Clear existing items in the container
+
+    // Define lists of buildings and items
+    let buildings = ["Building 1", "Building 2", "Building 3"];
+    let items = ["Item 1", "Item 2", "Item 3"];
+
+    // Populate the container based on the selected tab
+    let itemList;
+    if(type === "Combat"){
+        itemList = combat;
+    }
+    else if(type === "Resources"){
+        itemList = resources;
+    }
+    else{
+        itemList = decorations;
+    }
+    populateContainer("container1", itemList, type);
+}
+
+
+function populateContainer(containerId, itemList, type) {
     const container = document.getElementById(containerId);
 
     // Clear existing items
     container.innerHTML = '';
 
-    container.innerHTML = '<label><h3>' + "Buildings" + '</h3></label>';
+    container.innerHTML = '<label><h3>' + type + '</h3></label>';
 
     // Create and append items based on the itemList
     itemList.forEach((itemName, index) => {
@@ -29,11 +55,19 @@ function populateContainer(containerId, itemList) {
         else if(itemName === 'tower') {
             img.src = "/static/images/buildings/tower.png";
         }
+        else if(itemName === 'bush') {
+            img.src = "/static/images/buildings/bush.png";
+        }
         img.alt = itemName;
         item.appendChild(img);
         container.appendChild(item);
     });
 }
 
-// Call the function to populate the container
-populateContainer('container1', buildings);
+populateContainer("container1", combat, "Combat");
+
+function exitMenu() {
+    // Your code to handle closing the menu goes here
+    console.log("Menu closed");
+}
+
