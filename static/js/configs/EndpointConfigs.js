65a36f97bdc6ac7a260fd587b3ab2d09acafce88
Joren2087 <54290899+Joren2087@users.noreply.github.com>
2024-04-03 13:29:54 +0200

    Fixed API URL hardcoded to localhost

--- static/js/configs/EndpointConfigs.js
+++ static/js/configs/EndpointConfigs.js
@@ -1,5 +1,5 @@
 // export const API_URL = 'https://team2.ua-ppdb.me:8081';
-export const API_URL = 'http://127.0.0.1:5000';
+export const API_URL = window.location.origin;
 
 export const workerURI = './static/js/workers/ColliderWorker.js';
 
