2a237b267fa224fda5ad1c68686f0560edc81878
darik0d <70072471+darik0d@users.noreply.github.com>
2024-04-20 14:06:30 +0200

    Remove obsolete code gltf loader

--- static/js/Controller/AssetLoader.js
+++ static/js/Controller/AssetLoader.js
@@ -30,11 +30,7 @@ export class AssetLoader{
         //let extension = getFileExtension(path);
         let extension = path[1].slice(1);
         if(extension === "glb" || extension === "gltf"){
-            try{
-                return this.loadGLTF(path[0]);
-            }catch (e) {
-                return this.loadDRACOGLTF(path[0]);
-            }
+            return this.loadGLTF(path[0]);
         } else if(extension === "fbx"){
             return this.loadFBX(path[0]);
         } else if(extension === "png" || extension === "jpg") {
@@ -46,32 +42,6 @@ export class AssetLoader{
             throw new Error(`cannot load model with .${extension} extension`);
         }
     }
-    /**
-     * Load a gltf model
-     * @param {string} path path to the model
-     * @returns {*} the model and its animations
-     */
-    loadGLTF(path){
-        let loader = new GLTFLoader(this.loadingManager);
-        return loader.loadAsync(path, function (xhr) {
-            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        }).then((gltf) => {
-            let charModel;
-            let animations = null;
-
-            charModel = gltf.scene;
-            charModel.traverse(c => {
-                c.castShadow = true;
-            });
-            if(gltf.animations.length > 0){
-                animations = gltf.animations;
-                return {charModel, animations};
-            }
-            return {charModel};
-        },(err) => {
-            return this.loadDRACOGLTF(path);
-        });
-    }
 
     //TODO:: add timeout error handler
     /**
@@ -79,7 +49,7 @@ export class AssetLoader{
      * @param {string} path path to the model
      * @returns {*} the model and its animations
      */
-    loadDRACOGLTF(path){
+    loadGLTF(path){
         let loader = new GLTFLoader();
         let draco = new DRACOLoader();
         draco.setDecoderPath( './static/decoders/dracoloader/' );
